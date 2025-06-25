9899ed627db9c4cd1f5998735a313000470fb07a
dokudami <ysdzm75@gmail.com>
2025-06-25 20:10:13 +0900

    fix: export処理はいらないらしい

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
 
 const nextConfig: NextConfig = {
+  output: 'export',
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
   trailingSlash: true,
